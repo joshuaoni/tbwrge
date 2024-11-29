eb78d28789357c4509cb1cb250b48128381c9b75
Amiolemen David <david@piqy.co.uk>
2024-11-29 01:05:24 +0100

    ui fix

--- src/pages/dashboard/job-postings/current-openings.tsx
+++ src/pages/dashboard/job-postings/current-openings.tsx
@@ -1,31 +1,36 @@
-import { ShoppingBag } from "lucide-react";
+import { File, ShoppingBag, User } from "lucide-react";
 import React from "react";
 
 const CurrentOpenings = () => {
+  const [stats, setStats] = React.useState([
+    {
+      title: "Total Job Posts",
+      value: 12,
+      icon: <ShoppingBag />,
+    },
+    {
+      title: "Qualified Applicants",
+      value: 12,
+      icon: <User />,
+    },
+    {
+      title: "Total Applicants",
+      value: 300,
+      icon: <File />,
+    },
+  ]);
   return (
     <section>
       <div className="flex items-center space-x-8">
-        <div className="shadow-md rounded-2xl  justify-center p-4 bg-white h-28 flex flex-col w-full md:w-80">
-          <div className="flex items-center space-x-2">
-            <ShoppingBag />
-            <span className="text-sm font-light ">Total Job Posts</span>
+        {stats.map((stat, index) => (
+          <div className="shadow-md rounded-2xl  justify-center p-4 bg-white h-28 flex flex-col w-full md:w-80">
+            <div className="flex items-center space-x-2">
+              {stat.icon}
+              <span className="text-sm font-light ">{stat.title}</span>
+            </div>
+            <h1 className="text-2xl font-bold mt-4">{stat.value}</h1>
           </div>
-          <h1 className="text-2xl font-bold mt-4">{12}</h1>
-        </div>
-        <div className="shadow-md rounded-2xl justify-center p-4 bg-white h-28 flex flex-col w-full md:w-80">
-          <div className="flex items-center space-x-2">
-            <ShoppingBag />
-            <span className="text-sm font-light ">Qualified Applicants</span>
-          </div>
-          <h1 className="text-2xl font-bold mt-4">{12}</h1>
-        </div>
-        <div className="shadow-md rounded-2xl justify-center p-4 bg-white h-28 flex flex-col w-full md:w-80">
-          <div className="flex items-center space-x-2">
-            <ShoppingBag />
-            <span className="text-sm font-light ">Total Applicants</span>
-          </div>
-          <h1 className="text-2xl font-bold mt-4">{12}</h1>
-        </div>
+        ))}
       </div>
     </section>
   );
