d91dfa79c63ad30981b8f1628363b83ce040da2a
Olakotan Daniel <officialhorlakz@gmail.com>
2024-12-30 01:38:49 +0100

    feat(/pages/): adds React Query Devtools for better debugging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { Outfit } from "next/font/google"; // Corrected import path
 
@@ -24,6 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
           </main>
         </LocalizationProvider>
       </GoogleOAuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
