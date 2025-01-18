82b72673b2f2b0eca8b8750fdadb17dee550861c
Olakotan Daniel <officialhorlakz@gmail.com>
2025-01-18 23:00:04 +0100

    feat(vetting): refactor MetricCardProps to extend IMetric and update VettingResponse type

--- src/pages/dashboard/cv-tools/vetting/vetting.interface.ts
+++ src/pages/dashboard/cv-tools/vetting/vetting.interface.ts
@@ -1,9 +1,9 @@
-export interface MetricCardProps {
+export interface MetricCardProps extends IMetric {}
+
+export type VettingResponse = { metrics: IMetric[] }[];
+
+export interface IMetric {
   Metric: string;
   Score: number;
   Recommendation: string;
-}
-
-export interface VettingResponse {
-  0: { metrics: { Metric: string; Score: number; Recommendation: string }[] };
 }
