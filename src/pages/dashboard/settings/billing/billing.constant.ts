8c4aed5a9e708fa8534eab56879c55841a7bd319
Olakotan Daniel <officialhorlakz@gmail.com>
2025-01-22 10:50:28 +0100

    feat(billing): add BillingAddCard to billing screens

--- src/pages/dashboard/settings/billing/billing.constant.ts
+++ src/pages/dashboard/settings/billing/billing.constant.ts
@@ -1,7 +1,9 @@
+import BillingAddCard from "./add-card";
 import BillingChooseView from "./choose-bills";
 import BillingManageView from "./manage-bills";
 
 export const billingScreens = {
   manage: BillingManageView,
   choose: BillingChooseView,
+  add: BillingAddCard,
 };
