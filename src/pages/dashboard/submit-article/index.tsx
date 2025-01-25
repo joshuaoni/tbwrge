fc69c7043cfbab30be3cfea08fad2a95bfc6070a
Olakotan Daniel <officialhorlakz@gmail.com>
2025-01-25 12:33:19 +0100

    feat(submit-article): implement form handling and submission logic for article submission

--- src/pages/dashboard/submit-article/index.tsx
+++ src/pages/dashboard/submit-article/index.tsx
@@ -1,3 +1,12 @@
+import { useMutation } from "@tanstack/react-query";
+import classNames from "classnames";
+import { useState } from "react";
+
+import {
+  INITIAL_SUBMIT_ARTICLE_REQUEST_DATA,
+  SubmitAnArticleRequestData,
+  submitArticle,
+} from "@/actions/submit-article";
 import DashboardWrapper from "@/components/dashboard-wrapper";
 import {
   FeedbackSupportFileGroup,
@@ -8,9 +17,42 @@ import SubmitArticleFileGroup from "@/components/dashboard/submit-article/file-g
 import SubmitArticleRecordVoice from "@/components/dashboard/submit-article/record-voice";
 import CheckBoxInput from "@/components/settings/checkbox-input";
 import { urbanist } from "@/constants/app";
-import classNames from "classnames";
+import { useUserStore } from "@/hooks/use-user-store";
+import toast from "react-hot-toast";
 
 const DashboardSubmitArticlePage = () => {
+  const [formData, setFormData] = useState<SubmitAnArticleRequestData>(
+    INITIAL_SUBMIT_ARTICLE_REQUEST_DATA
+  );
+  const { userData } = useUserStore();
+
+  const [tc, setTc] = useState(false);
+
+  function handleSetFormData(
+    key: keyof SubmitAnArticleRequestData,
+    value: string | File | Blob
+  ) {
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  }
+
+  const submitArticleMutation = useMutation({
+    mutationFn: async (data: SubmitAnArticleRequestData) =>
+      await submitArticle(userData?.token ?? "", data),
+    onError: (error: any) => {
+      if (error.response?.data?.detail) {
+        error.response.data.detail.forEach((err: any) => {
+          toast.error(`${err.msg} at ${err.loc.join(" -> ")}`);
+        });
+      } else {
+        toast.error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Article submitted successfully");
+      setFormData(INITIAL_SUBMIT_ARTICLE_REQUEST_DATA);
+    },
+  });
+
   return (
     <DashboardWrapper>
       <h2 className={classNames(urbanist.className, "py-4 font-bold text-3xl")}>
@@ -21,9 +63,20 @@ const DashboardSubmitArticlePage = () => {
         <section className="px-4 py-6 w-full">
           <h3 className="font-bold">Article Details</h3>
           <div className="py-4 space-y-4">
-            <FeedbackSupportInputGroup label="Article Title" />
-            <FeedbackSupportTextareaGroup label="Article Content" />
-            <SubmitArticleFileGroup label="Upload Article (optional)" />
+            <FeedbackSupportInputGroup
+              label="Article Title"
+              value={formData.title}
+              onChange={(val) => handleSetFormData("title", val)}
+            />
+            <FeedbackSupportTextareaGroup
+              label="Article Content"
+              value={formData.content}
+              onChange={(val) => handleSetFormData("content", val)}
+            />
+            <SubmitArticleFileGroup
+              label="Upload Article (optional)"
+              onChange={(file) => handleSetFormData("article_upload", file)}
+            />
 
             <p className="relative uppercase text-center text-[#6D6D6D] py-6 px-4 after:absolute after:top-1/2 after:right-8 after:h-0.5 after:w-5/12 after:bg-[#E7E7E7] before:absolute before:top-1/2 before:left-8 before:h-0.5 before:w-5/12 before:bg-[#E7E7E7]">
               or
@@ -36,19 +89,49 @@ const DashboardSubmitArticlePage = () => {
           <h3 className="font-bold">Author Information</h3>
 
           <div className="py-4 space-y-4">
-            <FeedbackSupportInputGroup label="Full name" />
-            <FeedbackSupportInputGroup label="Job Title (optional)" />
-            <FeedbackSupportInputGroup label="Comapany (optional)" />
-            <FeedbackSupportFileGroup label="Upload Proflie Image" />
+            <FeedbackSupportInputGroup
+              label="Full name"
+              value={formData.name}
+              onChange={(val) => handleSetFormData("name", val)}
+            />
+            <FeedbackSupportInputGroup
+              label="Job Title (optional)"
+              value={formData.job_title}
+              onChange={(val) => handleSetFormData("job_title", val)}
+            />
+            <FeedbackSupportInputGroup
+              label="Comapany (optional)"
+              value={formData.company}
+              onChange={(val) => handleSetFormData("company", val)}
+            />
+            <FeedbackSupportFileGroup
+              label="Upload Proflie Image"
+              onChange={(file) => handleSetFormData("profile_image", file)}
+              file={formData.profile_image}
+            />
 
             <h3 className="text-lg font-bold py-3">Contact Information</h3>
 
-            <FeedbackSupportInputGroup label="Email" />
-            <CheckBoxInput label="Agree to terms and condtions" />
+            <FeedbackSupportInputGroup
+              label="Email"
+              value={formData.email}
+              onChange={(val) => handleSetFormData("email", val)}
+            />
+            <CheckBoxInput
+              label="Agree to terms and condtions"
+              value={tc}
+              onChange={(val) => setTc(val)}
+            />
 
             <div className="w-full flex justify-center">
-              <button className="px-3 py-3.5 bg-lightgreen text-sm text-white rounded-md font-semibold">
-                Submit Article
+              <button
+                disabled={!tc}
+                className="px-3 py-3.5 bg-lightgreen text-sm text-white rounded-md font-semibold disabled:opacity-70 disabled:cursor-not-allowed"
+                onClick={() => submitArticleMutation.mutate(formData)}
+              >
+                {submitArticleMutation.isPending
+                  ? "Submitting..."
+                  : "Submit Article"}
               </button>
             </div>
           </div>
