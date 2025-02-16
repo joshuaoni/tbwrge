22951af6fee7d18094d8769ea3eb3ba47ca9237d
Amiolemen David <david@piqy.co.uk>
2025-02-16 12:16:39 +0100

    update to the left side bar

--- src/components/left-side-bar.tsx
+++ src/components/left-side-bar.tsx
@@ -123,7 +123,7 @@ const LeftSideBar = () => {
         </div>
         <div className="h-[1px] w-full bg-[#A6CCB8] my-2" />
         <div className="flex flex-col ">
-          <div className="space-y-6 mb-4 w-full">
+          <div className="space-y-2 mb-2 w-full ">
             {extras.map((item) => {
               if (item.title === "Logout") {
                 return <LogoutModal />;
@@ -142,7 +142,7 @@ const LeftSideBar = () => {
         <div className="h-[1px] w-full bg-[#A6CCB8] my-2" />
         <div className="flex flex-col ">
           <h1 className="my-4 font-semibold text-[#6D6D6D] ml-8">User</h1>
-          <div className="space-y-6 mb-24 w-full">
+          <div className="space-y-2 mb-24 w-full">
             {userLeftSideItems.map((item) => {
               if (item.title === "Logout") {
                 return <LogoutModal />;
