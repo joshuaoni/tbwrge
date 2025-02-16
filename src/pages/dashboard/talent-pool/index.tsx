c527428a871fa13049e73ca6f16a9de48e5447db
Amiolemen David <david@piqy.co.uk>
2025-02-16 12:14:22 +0100

    update to the ui

--- src/pages/dashboard/talent-pool/index.tsx
+++ src/pages/dashboard/talent-pool/index.tsx
@@ -21,43 +21,43 @@ export default function TalentPool() {
     <DashboardWrapper>
       <span className="font-bold text-xl">Talent Pool</span>
       <div className="p-6  min-h-screen">
-        <div className=" mx-auto bg-white p-4 rounded-lg shadow">
+        <div className=" mx-auto bg-white p-4 rounded-lg ">
           <div className="flex items-center gap-3">
             <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mb-4">
               <input
                 type="text"
                 placeholder="Search for Keywords"
-                className="w-full p-2 border rounded-full bg-gray-100"
+                className="w-full p-2 border text-sm rounded-full bg-[#F0F0F0]"
               />
             </div>
             <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mb-4">
               <input
                 type="text"
                 placeholder="Location"
-                className="w-full p-2 border rounded-lg bg-gray-100"
+                className="w-full p-2 border text-sm rounded-lg bg-[#F0F0F0]"
               />
             </div>
             <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mb-4">
               <input
                 type="text"
                 placeholder="Skills"
-                className="w-full p-2 border rounded-lg bg-gray-100"
+                className="w-full p-2 border text-sm rounded-lg bg-[#F0F0F0]"
               />
             </div>
           </div>
 
-          <div className="border rounded-lg overflow-x-auto">
+          <div className="border rounded-lg overflow-x-auto bg-[#F0F0F0] p-4">
             <table className="w-full text-left text-sm">
-              <thead className="bg-gray-200">
-                <tr>
-                  <th className="p-3">Candidate Name</th>
-                  <th>Location</th>
-                  <th>Skills</th>
-                  <th>Most Recent Position</th>
-                  <th>Attachments</th>
+              <thead className="bg-[#D6D6D6] rounded-lg mb-4">
+                <tr className="rounded-lg">
+                  <th className="p-3 text-[#898989]">CANDIDATE NAME</th>
+                  <th className="text-[#898989]">Location</th>
+                  <th className="text-[#898989]">Skills</th>
+                  <th className="text-[#898989]">Most Recent Position</th>
+                  <th className="text-[#898989]">ATTACHMENTS</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="">
                 {candidates.map((candidate, index) => (
                   <tr
                     key={index}
@@ -66,7 +66,7 @@ export default function TalentPool() {
                       router.push(`/dashboard/talent-pool/candidate-profile`)
                     }
                   >
-                    <td className="p-3 font-medium whitespace-nowrap text-blue-500 underline">
+                    <td className="p-3 font-medium whitespace-nowrap text-[#000000] ">
                       {candidate.name}
                     </td>
                     <td className="whitespace-nowrap">{candidate.location}</td>
