1cab6316f0a03c3b7c2bb4aa5ac2cb6c2a34584c
Olakotan Daniel <officialhorlakz@gmail.com>
2025-02-17 19:58:00 +0100

    feat(hiring flow): implement dynamic hiring steps with improved select group component

--- src/components/dashboard/create-job/hiring-flow.tsx
+++ src/components/dashboard/create-job/hiring-flow.tsx
@@ -1,16 +1,60 @@
-import { useContext } from "react";
+import { useContext, useMemo, useState } from "react";
 
 import { CreateJobContext } from "@/providers/job-posting.context";
 import { ArrowLeft } from "lucide-react";
-import { DashboardInputGroup, DashboardSelectGroup } from "../input-group";
+import { DashboardInputGroup } from "../input-group";
+import { CreateJobHiringSelectGroup } from "./hiring-select-group";
 
 function CreateJobHiringFlow() {
   const ctx = useContext(CreateJobContext);
 
+  const [secondStep, setSecondStep] = useState("");
+  const [thirdStep, setThirdStep] = useState("");
+  const [fourthStep, setFourthStep] = useState("");
+
+  const stepActions = {
+    topFitScore: [
+      { label: "view screening email", onClick: () => {} },
+      { label: "view screening page", onClick: () => {} },
+    ],
+    topSelected: [{ label: "view interview email", onClick: () => {} }],
+    topFiltered: [{ label: "view rejection email", onClick: () => {} }],
+  };
+
+  function actionsVal(step: string) {
+    return step !== "" ? stepActions[step as keyof typeof stepActions] : [];
+  }
+
+  const selectedValues = [secondStep, thirdStep, fourthStep].filter(Boolean);
+
+  const isDisabled = (value: string) => selectedValues.includes(value);
+
+  const options = useMemo(
+    () => [
+      {
+        label: `Send Screening Email Top ${
+          100 - ctx.formData.minimum_fit_score
+        }% Candidates`,
+        value: "topFitScore",
+        disabled: isDisabled("topFitScore"),
+      },
+      {
+        label: "Send Screening Email Top selected Candidates",
+        value: "topSelected",
+        disabled: isDisabled("topSelected"),
+      },
+      {
+        label: "Send Screening Email Top filtered Candidates",
+        value: "topFiltered",
+        disabled: isDisabled("topFiltered"),
+      },
+    ],
+    [ctx.formData.minimum_fit_score, selectedValues]
+  );
+
   return (
     <div className="">
       <h3 className="text-3xl font-semibold pb-10">
-        {" "}
         <button onClick={() => ctx.prevScreen()} className="mr-4">
           <ArrowLeft />
         </button>
@@ -25,88 +69,37 @@ function CreateJobHiringFlow() {
           onChange={(val) => ctx.setFormData("minimum_fit_score", val)}
         />
 
-          <DashboardSelectGroup
-            className="pt-14"
-            title="Step 2"
-            options={[
-              {
-                label: "Send Screening Email Top 30% Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top selected Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top filtered Candidates",
-                value: "30_candidates",
-              },
-            ]}
-            defaultValue="Send Screening Email Top 30% Candidates"
-            onChange={function (val: string): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </section>
-        <section className="w-full space-y-4">
-          <DashboardSelectGroup
-            title="Step 3"
-            options={[
-              {
-                label: "Send Screening Email Top 30% Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top selected Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top filtered Candidates",
-                value: "30_candidates",
-              },
-            ]}
-            defaultValue="Send Screening Email Top Selected Candidates"
-            onChange={function (val: string): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <DashboardInputGroup
-            label="Please input the quantity of top candidates"
-            placeholder="70%"
-            className="w-1/3"
-          />
-
-          <DashboardSelectGroup
-            className="pt-14"
-            title="Step 4"
-            options={[
-              {
-                label: "Send Screening Email Top 30% Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top selected Candidates",
-                value: "30_candidates",
-              },
-              {
-                label: "Send Screening Email Top filtered Candidates",
-                value: "30_candidates",
-              },
-            ]}
-            defaultValue="Send Screening Email Top Filtered Candidates"
-            onChange={function (val: string): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </section>
-      </div>
+        {[
+          { val: secondStep, state: setSecondStep },
+          { val: thirdStep, state: setThirdStep },
+          { val: fourthStep, state: setFourthStep },
+        ].map((item, i) => (
+          <div className="space-y-4">
+            <CreateJobHiringSelectGroup
+              key={i}
+              title={"Step " + (i + 2)}
+              options={options}
+              defaultValue="Choose the next step of your hiring process"
+              onChange={(val) => item.state(val)}
+              actions={actionsVal(item.val)}
+            />
+            {item.val == "topSelected" && (
+              <DashboardInputGroup
+                label="Please input the quantity of top candidates"
+                placeholder="70%"
+                className="w-3/5"
+              />
+            )}
+          </div>
+        ))}
+      </section>
 
       <div className="w-full py-6 flex items-center justify-center">
         <button
           onClick={() => ctx.goTo("overview")}
           className="bg-[#009379] text-white px-8 py-2 rounded-lg"
         >
-          Save & Publish
+          Publish & View Job Post
         </button>
       </div>
     </div>
