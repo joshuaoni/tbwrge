faddee76329b9069988ab2e16d949f870d062800
Olakotan Daniel <officialhorlakz@gmail.com>
2025-02-18 18:17:16 +0100

    feat(create job): update create job screens to include screening component

--- src/constants/create-job.constant.ts
+++ src/constants/create-job.constant.ts
@@ -1,13 +1,13 @@
 import CreateJobCompanyDetails from "@/components/dashboard/create-job/company-details";
 import CreateJobHiringFlow from "@/components/dashboard/create-job/hiring-flow";
 import CreateJobJobDetails from "@/components/dashboard/create-job/job-details";
-import CreateJobOverview from "@/components/dashboard/create-job/overview";
+import CreateJobScreening from "@/components/dashboard/create-job/screening";
 
 export const createJobScreens = {
   intro: CreateJobCompanyDetails,
   job: CreateJobJobDetails,
   hiring: CreateJobHiringFlow,
-  overview: CreateJobOverview,
+  screening: CreateJobScreening,
 };
 
 export const INITIAL_CREATE_JOB_FORM_DATA = {
