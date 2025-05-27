fccc86510dcc923ad874c207e28659fbd513aaaa
Joshua Oni <joshuaoni@punch.agency>
2025-05-27 14:15:26 +0100

    sitemap added.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,11 @@ module.exports = {
   siteUrl: "https://www.candivet.com",
   generateRobotsTxt: true, // optional if you want robots.txt generated
   exclude: [
+    "/admin",
     "/admin/*",
     "/home/components/*", // exclude all component/internal wrapper URLs
-    "/blog-post",
+    "/blog-posts",
+    "/dashboard/candidates",
     "/dashboard/dashboard-home",
     "/dashboard/job-board/components/*",
     "/dashboard/job-board/job-board-filter",
