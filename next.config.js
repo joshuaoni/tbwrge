73cfd690c2ecf60c1839c5dd1a56927762b985d6
Joshua Oni <joshuaoni@punch.agency>
2025-06-07 09:45:27 +0100

    reset next config

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@ const nextConfig = {
   images: {
     domains: ["bbc.com", "res.cloudinary.com", "ui-avatars.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "unsafe-none", // ✅ allows postMessage
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none", // ✅ optional but recommended in this case
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
