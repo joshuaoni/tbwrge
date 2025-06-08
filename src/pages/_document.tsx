7eb0a61822b44353c3b8691a49171b836d8afa61
Joshua Oni <joshuaoni@punch.agency>
2025-06-08 11:59:54 +0100

    candivet tab logo changed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,9 @@ export default function Document() {
           content="Candivet is the #1 global AI-powered platform designed to simplify hiring for recruiters and job seekers alike. Whether you're looking to find top talent or land your dream job, we optimize every step of the process."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/footer-logo.png" />
+        <link rel="apple-touch-icon" href="/footer-logo.png" />
+        <meta name="theme-color" content="#065844" />
       </Head>
       <body className="antialiased">
         <Main />
