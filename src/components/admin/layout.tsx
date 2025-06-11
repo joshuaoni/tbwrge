5631818784744d00e7e0d2c9ef0e99f361340192
Joshua Oni <joshuaoni@punch.agency>
2025-06-11 14:23:26 +0100

    admin dashboard responsiveness fixed

--- src/components/admin/layout.tsx
+++ src/components/admin/layout.tsx
@@ -5,10 +5,13 @@ import AdminDashboardSidebar from "./sidebar";
 const AdminDashboardLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="flex">
+      <div className="w-[300px] shrink-0" />
       <AdminDashboardSidebar />
-      <main className="flex-1 ml-[300px]">
-        <AdmindashboardHeader />
-        <div className="p-6">{children}</div>
+      <main className="flex-1 box-border overflow-x-hidden">
+        <div className="max-w-[1800px] mx-auto">
+          <AdmindashboardHeader />
+          <div className="py-6 px-6">{children}</div>
+        </div>
       </main>
     </div>
   );
