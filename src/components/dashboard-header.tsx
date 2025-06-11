6bdb23bba30c056b2abacbe935e098419aa4223a
Joshua Oni <joshuaoni@punch.agency>
2025-06-11 10:02:31 +0100

    dashboard header responsiveness fixed

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -23,9 +23,9 @@ const DashboardHeader = ({
 
   return (
     <div
-      className={`${outfit.className} ml-[260px] w-[calc(100vw-260px)] border-b h-20 z-20 bg-white flex items-center px-4 pl-12 fixed top-0`}
+      className={`${outfit.className} w-full border-b h-20 z-20 bg-white flex items-center px-4`}
     >
-      <div className="flex w-full items-center justify-between mt-2">
+      <div className="flex w-full items-center justify-between mt-2 pl-12">
         {/* Search */}
         <div className="flex items-center border px-2 bg-[#F0F0F0] rounded-full mr-4">
           <Search color="#898989" />
